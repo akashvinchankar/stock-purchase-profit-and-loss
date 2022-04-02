e7c9da7eebd84e40c927337ec1061279ebfb9195
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-04-02 12:32:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ function submitHandler(){
 function calculateProfitAndLoss(initial, quantity, current){
     if(initial > current) {
         var loss = (initial - current) * quantity 
-        var lossPercentage = (loss / initial) * 100;
+        var lossPercentage = 100 - ((loss / initial) * 100);
         
         showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}%`);
     }
     else if (current > initial) {
         var profit = (current - initial) * quantity 
-        var profitPercentage = (profit / initial) * 100;
+        var profitPercentage = ((profit / initial) * 100) - 100;
         
         showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}%`);
     }
